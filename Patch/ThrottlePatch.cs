144fb61bf2e7192d55128581b1897a87144f2b18
TruffleWolf <life4pylo@gmail.com>
2025-03-28 11:26:12 +0000

    Initial Implementation

---
+++ Patch/ThrottlePatch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+using Rewired;
+
+namespace NuclearOption_Throttle_Fix.Patch
+{
+    [HarmonyPatch(typeof(PilotPlayerState))]
+    internal class ThrottlePatch
+    {
+
+        [HarmonyPatch("PlayerAxisControls")]
+        [HarmonyPrefix]
+        static void InfoPatch(ref float ___simulatedThrottle,out float __state)
+        {
+
+            __state = ___simulatedThrottle;
+
+        }
+
+        [HarmonyPatch("PlayerAxisControls")]
+        [HarmonyPostfix]
+        static void InfoPatch2(ref ControlInputs ___controlInputs, ref Rewired.Player ___player,ref float ___simulatedThrottle,float __state)
+        {
+            float simThrottle = __state;
+            float num2 = Mathf.Clamp(___player.GetAxisRaw("Throttle"), -1f, 1f);
+
+            simThrottle += Mathf.Clamp(num2, 0f - (Time.deltaTime*Mathf.Abs(num2)), (Time.deltaTime * Mathf.Abs(num2)));
+
+            ___simulatedThrottle = Mathf.Clamp01(simThrottle);
+            ___controlInputs.throttle = ___simulatedThrottle;
+            
+        }
+
+
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using NuclearOption_Throttle_Fix.Patch;
+
+namespace NuclearOption_Throttle_Fix
+{
+    [BepInPlugin(modGUID,modName,modVersion)]
+    public class ThrottleFixBase :BaseUnityPlugin
+    {
+        private const string modGUID = "Truffle.NOThrottleFix";
+        private const string modName = "Trigger Throttle Fix";
+        private const string modVersion = "0.30.0.1";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        internal static ThrottleFixBase Instance;
+        internal static ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            harmony.PatchAll(typeof(ThrottleFixBase));
+            harmony.PatchAll(typeof(ThrottlePatch));
+            mls.LogInfo("Trigger Throttle Fix Started");
+        }
+    }
+}
